9a56b328000bcf6954bb5faba432bff21f9a1e76
Nathan Miguel <nathan.cm77@gmail.com>
2025-12-06 15:12:17 -0300

    updated fumadocs version

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,22 +1,9 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 
-/**
- * Shared layout configurations
- *
- * you can customise layouts individually from:
- * Home Layout: app/(home)/layout.tsx
- * Docs Layout: app/docs/layout.tsx
- */
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: (
-        <>
-          Internet Protocol
-        </>
-      ),
+      title: 'Internet Protocol',
     },
-    // see https://fumadocs.dev/docs/ui/navigation/links
-    links: [],
   };
 }
